f68717c26ae6e4ce7f76be6e3c3db6c9e2f2aecb
Alex Rael <alprael22@gmail.com>
2018-10-03 22:53:46 -0600

    fixed summations of all values below or equal to 1000. Included if statements to subtract values that were multiples of 3 or 5.

--- src/edu/cnm/deepdive/SumMult.java
+++ src/edu/cnm/deepdive/SumMult.java
@@ -7,20 +7,16 @@ public class SumMult {
     int sum = 0;
     int i = 1;
     while (i <= 1000) {
-      i++;
       sum = sum + i;
-      if ((i % 3 == 0) && (i % 5 == 0)){
-        int mult = sum - i;
-        if (i % 3 == 0){
-          int t = mult - i;
-          if (i % 5 == 0) {
-            int x = t - i;
-            int v = sum - x;
-            System.out.println(v + " " + "is the summation of all the multiples of 3 and 5 below 1000");
-          }
-        }
+      i++;
+      System.out.println(sum);
+      if ((i % 3 == 0) || (i % 5 == 0)) {
+        sum = sum - i;
+        System.out.println(sum);
+        
       }
     }
   }
 }
+
 
