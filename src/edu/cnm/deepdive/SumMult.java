7f67a260c72ea83d9deb90e5209c7dc8fc18c335
Alex Rael <alprael22@gmail.com>
2018-10-03 19:17:47 -0600

    tweaks to multiples summation

--- src/edu/cnm/deepdive/SumMult.java
+++ src/edu/cnm/deepdive/SumMult.java
@@ -8,14 +8,15 @@ public class SumMult {
     int i = 1;
     while (i <= 1000) {
       i++;
-      sum = (sum + i);
+      sum = sum + i;
       if ((i % 3 == 0) && (i % 5 == 0)){
         int mult = sum - i;
         if (i % 3 == 0){
           int t = mult - i;
           if (i % 5 == 0) {
             int x = t - i;
-            System.out.println(x - 1);
+            int v = sum - x;
+            System.out.println(v + " " + "is the summation of all the multiples of 3 and 5 below 1000");
           }
         }
       }
