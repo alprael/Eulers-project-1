a74f8ea6f866c4224f0706f947fdeb7d955adef8
Alex Rael <alprael22@gmail.com>
2018-10-05 08:14:41 -0600

    tweaked the process of the method

--- src/edu/cnm/deepdive/SumMult.java
+++ src/edu/cnm/deepdive/SumMult.java
@@ -6,15 +6,16 @@ public class SumMult {
   public static void main(String[] args) {
     int sum = 0;
     int i = 1;
-    while (i <= 1000) {
-      sum = sum + i;
-      i++;
+    while (i < 1000) {
+      //sum = sum + i;
       //System.out.println(sum);
       if ((i % 3 == 0) || (i % 5 == 0)) {
         sum = sum + i;
-        System.out.println(sum - 500500);
+        //System.out.println(sum - 500500);
       }
+      i++;
     }
+    System.out.println(sum);
   }
 }
 
